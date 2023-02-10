d484491f0f805e5b93eeccbaeebc681243cd2881
wachaidickson@gmail.com <wachaidickson@gmail.com>
2023-02-10 05:48:52 -0800

    Update base_model file by adding *args and **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,21 @@ from datetime import datetime
 
 class BaseModel:
     """All attributes and methods"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """"Defines the public
         instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    if k in ["created_at", "updated_at"]:
+                        v = datetime.\
+                                strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, k, v)
 
     def __str__(self):
         """A string representation of an object"""
