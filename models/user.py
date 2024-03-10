13eef32c69dd7d96807ea6680881f1ecc485ca87
DicksonWachai <wachaidickson@gmail.com>
2024-03-11 02:25:39 +0300

    Add user file

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# This script displays user attributes
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User attributes
+    email (str): The email of the user
+    password (str): The password of the user
+    first_name (str): The first name of the user
+    last_name (str): The last name of the user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
